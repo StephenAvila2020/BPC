2bf477e1e79408e51344a49e365a2b3deec66bbc
Alexander Griffiths <67096118+alexmgriffiths@users.noreply.github.com>
2022-07-26 22:37:16 -0500

    Feature/added boo (#12)
    * Fixes docker arg
    
    * Changed app port
    
    * Update github-ci to deploy to AWS EKS
    
    * Add image pull policy: Always
    
    * Fixes image tag and add kube config
    
    * Added spacing
    
    * Added boo to index

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
@@ -11,6 +11,10 @@ const app: Express = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
+
+app.get('/', (_req: Request, res: Response) => {
+  return res.status(200).send("Boo!");
+})
 
 app.use(
     "/docs",
